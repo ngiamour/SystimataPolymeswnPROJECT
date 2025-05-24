91955593bb84d8ad0625ec5f7418df69037adb4a
ngiamour <ngiamour8@gmail.com>
2025-05-24 19:32:03 +0300

    Added Volume Settings
    Added the ability to change the volume of music and SFX. Will change across the game and save on exit

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,13 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
     public GameObject accessibilityOptionsPanel;
-    
+
+    [SerializeField] private AudioMixer mixer;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider sfxSlider;
+
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            SetMusicVolume();
+            SetSFXVolume();
+        }
+    }
+
     public void LoadScene(string sceneToLoad)
     {
         SceneManager.LoadScene(sceneToLoad);
@@ -47,6 +66,30 @@ public class MainMenu : MonoBehaviour
     public void OnAccessibilityIconsToggleChanged(bool isOn)
     {
         AccessibilityManager.Instance.iconsAboveObjects = isOn;
+    }
+
+    public void SetMusicVolume()
+    {
+        float volume = musicSlider.value;
+        mixer.SetFloat("music", Mathf.Log(volume)*20);
+        
+        PlayerPrefs.SetFloat("musicVolume",volume);
+    }
+    
+    public void SetSFXVolume()
+    {
+        float volume = sfxSlider.value;
+        mixer.SetFloat("sfx", Mathf.Log(volume)*20);
+        
+        PlayerPrefs.SetFloat("sfxVolume",volume);
+    }
+
+    private void LoadVolume()
+    {
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+        sfxSlider.value = PlayerPrefs.GetFloat("sfxVolume");
+        SetMusicVolume();
+        SetSFXVolume();
     }
 
     /*
