5068dd0edb6637ea2201b11370a1fe890475bc34
ngiamour <ngiamour8@gmail.com>
2025-05-24 20:12:42 +0300

    Tweaked Main Menu
    Change some things in the main menu.
    Added intro music (x2)
    Tweaked some text scale numbers

---
+++ Assets/Scripts/PlayNextAudio.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayNextAudio : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    public AudioClip clip;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    public void PlaySoundNow()
+    {
+        _audioSource.PlayOneShot(clip,0.8f);
+    }
+}
